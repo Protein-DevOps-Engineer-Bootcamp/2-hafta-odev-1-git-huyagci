755505c970aff701b45981aeed4ae69e788b508a
Hasan Umut Yağcı <hasanumutyagci@gmail.com>
2022-06-01 00:29:41 +0300

    Append header to build_helper.sh

--- shared/scripts/build_helper.sh
+++ shared/scripts/build_helper.sh
@@ -1,1 +1,10 @@
 #! /bin/bash
+#############################################
+# Script Name   : Build Helper              #
+# File          : build_helper.sh           #
+# Usage         : ./build_helper.sh         #
+# Created       : 01/06/2022                #
+# Author        : Hasan Umut Yagci          #
+# Email         : hasanumutyagci@gmail.com  #
+#############################################
+
