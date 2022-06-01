83ba8c9aa0465099533ab46f21b53cc1ecb6924d
Hasan Umut Yağcı <hasanumutyagci@gmail.com>
2022-06-01 07:09:48 +0300

    Update exports to echoes

--- shared/scripts/bootstrap.sh
+++ shared/scripts/bootstrap.sh
@@ -13,8 +13,7 @@ sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk-18/bin/
 sudo update-alternatives --config java
 
 # Set JAVA_HOME
-export JAVA_HOME=/usr/lib/jvm/jdk-18
-
+echo "JAVA_HOME=/usr/lib/jvm/jdk-18" >> /etc/environment
 source /etc/environment
 
 # Get Maven
@@ -27,10 +26,19 @@ sudo tar -xf /usr/apache-maven-3.8.5-bin.tar.gz -C /usr
 sudo update-alternatives --install /usr/bin/mvn maven /usr/apache-maven-3.8.5/bin/mvn 1001
 
 # Set Maven Environment Variables
-export MAVEN_HOME=/usr/apache-maven-3.8.5
-export M2_HOME=/usr/apache-maven-3.8.5
+echo "MAVEN_HOME=/usr/apache-maven-3.8.5" >> /etc/environment
+echo "M2_HOME=/usr/apache-maven-3.8.5" >> /etc/environment
 export PATH=${MAVEN_HOME}/bin:${PATH}
 
 # Cleanup
 sudo rm -f /tmp/jdk-18_linux-x64_bin.deb
-sudo rm -f /usr/apache-maven.3.8.5-bin.tar.gz
+sudo rm -f /usr/apache-maven-3.8.5-bin.tar.gz
+
+# Git - will be edited later
+
+# git config --global user.email "example@example.com"
+# git config --global user.name "example"
+# git init
+# git add --all
+# git commit -m "Initial commit"
+# ?git branch new branch?
