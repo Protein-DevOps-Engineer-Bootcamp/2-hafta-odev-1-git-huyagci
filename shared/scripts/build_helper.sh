541adcfca2dc4d13cfa242828f455b06f62af6de
Hasan Umut Yağcı <hasanumutyagci@gmail.com>
2022-06-02 03:59:04 +0300

    Activate usage msg of the build_helper.sh

--- shared/scripts/build_helper.sh
+++ shared/scripts/build_helper.sh
@@ -16,19 +16,29 @@ TARGET_DIR=/opt/project/java
 # CD to target directory if executed from another directory
 cd $TARGET_DIR
 
-BUILD="mvn package"
+BUILD="mvn package -Dmaven.test.skip=true"
 
 USAGE_MSG="
     Usage: $(basename $0) [OPTIONS]
     
     OPTIONS:
-    [-b <branch_name>]      Branch name must be provided from the user. If not on the specified branch switch branch then build.
-    [-d <true|false>]       Enable|Disable debug mode. Default: DISABLED Must be taken from the user.
-    [-f <zip|tar>]          Compress format of the artifact. Must be zip or tar.gz. Else break. (branch_name.zip|tar.gz)
-    [-n <new_branch>]       Create a new branch
-    [-p <artifact_path>]    Copy compressed artifacts to given path.
-    [-t <true|false>]       Run or skip tests.
+    [-b <branch_name>      Branch name must be provided from the user. If not on the specified branch switch branch then build]
+    [-d <true|false>       Enable|Disable debug mode. Default: DISABLED Must be taken from the user]
+    [-f <zip|tar>          Compress format of the artifact. Must be zip or tar.gz. Else break. (branch_name.zip|tar.gz)]
+    [-n <new_branch>       Create a new branch]
+    [-p <artifact_path>    Copy compressed artifacts to given path]
+    [-t <true|false>       Run or skip tests]
 "
+
+usage() {
+    echo "${USAGE_MSG}"
+    exit 1
+}
+
+if [ "$#" -lt 1 ]
+then
+    usage
+fi
 
 eval "$BUILD"
 
